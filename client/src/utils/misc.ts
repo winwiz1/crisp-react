1f01a6b57625017371dbdde2882bddf5dacd993a
winwiz1 <winwiz@gmail.com>
2021-10-13 21:33:16 +1100

    Client: Fix for getCanonical()

--- client/src/utils/misc.ts
+++ client/src/utils/misc.ts
@@ -1,12 +1,18 @@
 import { isServer } from "./postprocess/misc";
 import * as SPAs from "../../config/spa.config";
 
+const regexPath1Literal = `/${SPAs.getRedirectName()}(.html)?$`;
+const regexPath1 = new RegExp(regexPath1Literal, "i");
+const regexPath2 = new RegExp(/\.html$/, "i");
+
 export const getCanonical = (pagePath?: string): string|undefined => {
   if (isServer()) {
     return undefined;
   }
   // eslint-disable-next-line no-extra-boolean-cast
-  return !!pagePath? (window.location.origin + pagePath) : window.location.href;
+  let ret = !!pagePath? (window.location.origin + pagePath) : window.location.href;
+  ret = ret.replace(regexPath1, "/").replace(regexPath2, "");
+  return ret;
 }
 
 export const getTitle = (pageTitle?: string): string => {
